a20753511a8c28f798e1641d7c6d7cbfbc7de459
vincecachi <vincentcachila@gmail.com>
2020-02-28 15:06:41 +0800

    asd

--- index.php
+++ index.php
@@ -7,13 +7,12 @@
 <?php 
 //Total In Stocks 
 $account = $_SESSION['username'];
-$qry = "SELECT * FROM area_center WHERE username = '$account'";
+$qry = "SELECT * FROM stockist WHERE username = '$account'";
 $result = mysqli_query($link, $qry) or die(mysqli_error($link));
 if (mysqli_num_rows($result) > 0) {
   while($rows = mysqli_fetch_array($result)){
      $username = $rows['username'];
     $warehouse = $rows['warehouse'];
-    
   }
    
 }
